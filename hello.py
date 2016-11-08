cb1d34107702a202c0adcf99e27f32ad9dc32c67
nomad9000 <nomad9000@mail.ru>
2016-11-08 10:53:25 -0500

    second ex

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 def wsgi_application(environ, start_response):
     query = environ.get('QUERY_STRING')
-    if query:
-        body = query.replace('&', '\n')
+    body = query.replace('&', '\n')
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
