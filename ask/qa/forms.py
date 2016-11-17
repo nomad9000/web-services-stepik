937209366a369f233c31e9b6f98e3e085c8e8714
nomad9000 <nomad9000@mail.ru>
2016-11-17 07:13:46 -0500

    fourth ex

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -4,8 +4,8 @@ from models import Question, Answer
 class AskForm(forms.Form):
     title = forms.CharField(max_length=256)
     text = forms.CharField(widget=forms.Textarea)
-    def clean(self):
-        return self.cleaned_data
+    #def clean(self):
+        #return self.cleaned_data
     def save(self):
         #q = Question(**self.cleaned_data)
         #q.save()
@@ -19,8 +19,8 @@ class AskForm(forms.Form):
 class AnswerForm(forms.Form):
     text = forms.CharField()
     question = forms.IntegerField()
-    def clean(self):
-        return self.cleaned_data
+    #def clean(self):
+        #return self.cleaned_data
     def save(self):
         #a = Answer(**self.cleaned_data)
         #a.save()
